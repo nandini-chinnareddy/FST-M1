8009330de0b40c7633efa007374756265a51436c
nandini-chinnareddy <nandini.chinnareddy@ibm.com>
2025-09-29 10:45:41 +0530

    Create activity7.java

---
+++ Selenium/Activities/activity7.java
@@ -0,0 +1,27 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class activity7 {
+	public static void main(String[] args) {
+        
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/dynamic-controls");
+        System.out.println("Page title: " + driver.getTitle());
+        
+        WebElement textbox = driver.findElement(By.id("textInput"));
+        System.out.println("Input field is enabled: " + textbox.isEnabled());
+        
+        driver.findElement(By.id("textInputButton")).click();
+       
+        System.out.println("Input field is enabled: " + textbox.isEnabled());
+        textbox.sendKeys("Normal text");
+        System.out.println(textbox.getDomProperty("value"));
+
+        driver.quit();
+
+}
+}
