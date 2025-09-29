08af8bfdf5a5b2c8aab11f1930dd50dab3781229
nandini-chinnareddy <nandini.chinnareddy@ibm.com>
2025-09-29 09:41:41 +0530

    Create activity10.py

---
+++ Python/Activities/activity10.py
@@ -0,0 +1,7 @@
+num_tuple = (1011, 20, 33, 58, 55)
+print("Given list is ", num_tuple)
+
+print("Elements that are divisible by 5:")
+for num in num_tuple:
+    if (num % 5 == 0):
+        print(num)
