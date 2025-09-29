3a16d6687f0e57928a8cbddfc9d9348ea7ea5a77
nandini-chinnareddy <nandini.chinnareddy@ibm.com>
2025-09-29 10:27:29 +0530

    Create activity12.java

---
+++ Selenium/Activities/activity12.java
@@ -0,0 +1,27 @@
+package activities;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class activity12 {
+	public static void main(String[] args) {
+        
+       WebDriver driver = new FirefoxDriver();
+       WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       
+       driver.get("https://www.training-support.net/webelements/dynamic-content");
+       System.out.println("Page title: " + driver.getTitle());
+       
+       driver.findElement(By.id("genButton")).click();
+       if (wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("word"), "porch"))) {
+           System.out.println("Word found: " + driver.findElement(By.id("word")).getText());
+       }
+       driver.quit();
+       
+
+}
+}
