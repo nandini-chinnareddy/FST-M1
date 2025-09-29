a09bdbe7069cf1e914c6e21ce9c568495f563d76
nandini-chinnareddy <nandini.chinnareddy@ibm.com>
2025-09-29 09:40:32 +0530

    Create activity8.py

---
+++ Python/Activities/activity8.py
@@ -0,0 +1,11 @@
+numList = [11, 20, 30, 40, 1021]
+print("Given list is ", numList)
+ 
+firstElement = numList[0]
+
+lastElement = numList[-1]
+ 
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
