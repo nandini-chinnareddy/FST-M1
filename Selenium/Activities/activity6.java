c20593c0b436c593378eff1b286ece9a065e97fd
nandini-chinnareddy <nandini.chinnareddy@ibm.com>
2025-09-29 10:44:54 +0530

    Create activity6.java

---
+++ Selenium/Activities/activity6.java
@@ -0,0 +1,24 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class activity6 {
+	public static void main(String[] args) {
+        
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://training-support.net/webelements/dynamic-controls");
+        
+        System.out.println("Page title: " + driver.getTitle());
+        
+        WebElement checkbox = driver.findElement(By.id("checkbox"));
+        checkbox.click();
+        System.out.println("Checkbox is selected: " + checkbox.isSelected());
+        checkbox.click();
+        System.out.println("Checkbox is selected: " + checkbox.isSelected());
+        driver.quit();
+
+}
+}
